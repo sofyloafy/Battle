cf21f0dd00c0b39b8cdc1568a3799642760459d1
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-26 16:51:24 +0100

    add avility to display images

--- app.rb
+++ app.rb
@@ -2,9 +2,26 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/' do
-  'Hello!'
+  'heya!'
 end
 
-get '/' do
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
+
+
+get '/secret' do
   'this is secret'
+end
+
+get '/houses' do
+  "<div>
+  <img src=https://davidburr.co.uk/wp-content/uploads/2018/11/Victorian-Properties-1050x550.jpg>
+  </div>"
+end
+
+post '/hi' do
+"hi"
 end
