33fc3dc39cd95b27efff1e01d61f624cfd7d8c1b
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-28 16:35:02 +0100

    Entering the players. Testing

--- Battle_3/app.rb
+++ Battle_3/app.rb
@@ -2,11 +2,17 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  get '/' do
-    "Testing infrastructure working!"
-  end
+  # get '/' do
+  #   "Testing infrastructure working!"
+  # end
 
   get '/' do
-    erb(:index)
+    erb :index
   end
+
+  post '/name' do
+    @name_1 = params[:name_1]
+    erb :play
+  end
+
 end
--- Battle_3/spec/features/testing_spec.rb
+++ Battle_3/spec/features/testing_spec.rb
@@ -1,14 +1,13 @@
-feature 'has test infrastructure' do
-  scenario 'can run and look at page content' do
-   visit '/'
-     expect(page).to have_content 'Testing infrastructure working!'
-  end
-
+feature 'enter names submitting names' do
+  # scenario 'can run and look at page content' do
+  #  visit '/'
+  #    expect(page).to have_content 'Testing infrastructure working!'
+  # end
   scenario 'players to fill in their names' do 
     visit '/'
-     fill_in 'Name_1', with: 'test_name'
-     fill_in 'Name_2', with: 'test_name_2'
+     fill_in :name_1, with: 'test_name'
+     fill_in :name_2, with: 'test_name_2'
      click_button 'Enter players'
-     expect(page).to have_text('test_name test_name_2')
+     expect(page).to have_text('test_name vs. test_name_2')
   end
 end
