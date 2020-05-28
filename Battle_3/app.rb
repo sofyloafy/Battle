895c6675e1d7b7c2a430f6240b7b09af692f59fb
Sophie <sophiebrown438@gmail.com>
2020-05-28 16:48:54 +0100

    Refactored form submission page and passing tests

--- Battle_3/app.rb
+++ Battle_3/app.rb
@@ -1,17 +1,13 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-
-  # get '/' do
-  #   "Testing infrastructure working!"
-  # end
-
   get '/' do
     erb :index
   end
 
   post '/name' do
     @name_1 = params[:name_1]
+    @name_2 = params[:name_2]
     erb :play
   end
 
--- Battle_3/config.ru
+++ Battle_3/config.ru
@@ -1,2 +1,3 @@
 require './app.rb'
+
 run Battle
